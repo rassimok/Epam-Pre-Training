aa0bd285637541fcc4489391edf146cd3c00020d
rassimok <komissar.bsu@gmail.com>
2018-02-05 00:48:04 +0300

    Add Task 2

---
+++ Task2/MoodSensor.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class MoodSensor {
+
+	public static final ArrayList<String> SMILES_ARRAY = new ArrayList<String>();
+	static {
+		SMILES_ARRAY.add("(^_^)");
+		SMILES_ARRAY.add(";’-)))");
+		SMILES_ARRAY.add("(_|_)");
+		SMILES_ARRAY.add(":)");
+		SMILES_ARRAY.add(":DDD");
+		SMILES_ARRAY.add(":(");
+		SMILES_ARRAY.add(";(");
+		SMILES_ARRAY.add(";)");
+		SMILES_ARRAY.add("(^o^)");
+		SMILES_ARRAY.add("(*-*)");
+	}
+
+	public static String getMood() {
+		return SMILES_ARRAY.get((int) (Math.random() * 10));
+	}
+
+}
