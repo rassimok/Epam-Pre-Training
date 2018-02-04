9f611ecf44ff488e6d6eddbcae6500a00cd2e611
rassimok <komissar.bsu@gmail.com>
2018-02-05 00:23:45 +0300

    Update
    add Task 2 Ex 1

--- Task2/Main.java
+++ Task2/Main.java
@@ -2,6 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
+		//Task 2 Exercise 1
+		int N = 299;
+		System.out.println("nomberOfHeads: " + Ex1Task2.getNomberOfHeads(N));
+		System.out.println("nomberOfEyes: " + Ex1Task2.getNomberOfEyes(N));
+		
+		
 		//Task 2 Exercise 4
 		System.out.println(DateByRassimok.getNextDayDate(1, 2, 1989));
 		//Task 2 Exercise 5
