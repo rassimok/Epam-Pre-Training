1c01ac22c4679f6d1516acd37c10f52a688f3acb
rassimok <komissar.bsu@gmail.com>
2018-02-05 00:16:55 +0300

    Ubdate Main with 5th Ex

--- Task2/Main.java
+++ Task2/Main.java
@@ -4,7 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		//Task 2 Exercise 4
 		System.out.println(DateByRassimok.getNextDayDate(1, 2, 1989));
-		
+		//Task 2 Exercise 5
+		System.out.println(NumbersInWords.getNumbersInWords(67));
 	}
 
 }
