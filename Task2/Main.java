ec86f9a1a92341c0553b017e394a4b7988fb03a6
rassimok <komissar.bsu@gmail.com>
2018-02-05 00:26:46 +0300

    Update Main
    Add Task 2 Ex2

--- Task2/Main.java
+++ Task2/Main.java
@@ -6,7 +6,12 @@ public class Main {
 		int N = 299;
 		System.out.println("nomberOfHeads: " + Ex1Task2.getNomberOfHeads(N));
 		System.out.println("nomberOfEyes: " + Ex1Task2.getNomberOfEyes(N));
-		
+		//Task 2 Exercise 2
+		char c='x';
+		System.out.println(c + " is vowel?(Version1) - " + Ex2Task2.isVowelVersion1(c));
+		System.out.println(c + " is vowel?(Version2) - " + Ex2Task2.isVowelVersion2(c));
+		System.out.println(c + " is vowel?(Version3) - " + Ex2Task2.isVowelVersion3(c));
+		System.out.println(c + " is vowel?(Version4) - " + Ex2Task2.isVowelVersion4(c));
 		
 		//Task 2 Exercise 4
 		System.out.println(DateByRassimok.getNextDayDate(1, 2, 1989));
