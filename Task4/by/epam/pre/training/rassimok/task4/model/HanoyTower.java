d467ad854ade558097375ea79286ce33eb599149
rassimok <komissar.bsu@gmail.com>
2018-02-21 14:21:25 +0300

    update HanoyTower

--- Task4/by/epam/pre/training/rassimok/task4/model/HanoyTower.java
+++ Task4/by/epam/pre/training/rassimok/task4/model/HanoyTower.java
@@ -1,15 +1,17 @@
 package by.epam.pre.training.rassimok.task4.model;
 
 public class HanoyTower {
-	private static StringBuilder displacementHanoyTower = new StringBuilder();
+	public static String countHanoyTowerDisplacement(int nomberOfRings, char start, char finish, char temp) {
+		return countHanoyTowerDisplacement(nomberOfRings, start, finish, temp, new StringBuilder()).toString();
+	}
 
-	public static StringBuilder countHanoyTowerDisplacement(int nomberOfRings, char start, char finish, char temp) {
+	private static StringBuilder countHanoyTowerDisplacement(int nomberOfRings, char start, char finish, char temp,
+			StringBuilder displacementHanoyTower) {
 		if (nomberOfRings > 0) {
-			countHanoyTowerDisplacement(nomberOfRings - 1, start, temp, finish);
+			countHanoyTowerDisplacement(nomberOfRings - 1, start, temp, finish, displacementHanoyTower);
 			displacementHanoyTower.append(start + "-->" + finish + "\n");
-			countHanoyTowerDisplacement(nomberOfRings - 1, temp, finish, start);
+			countHanoyTowerDisplacement(nomberOfRings - 1, temp, finish, start, displacementHanoyTower);
 		}
 		return displacementHanoyTower;
 	}
-
 }
